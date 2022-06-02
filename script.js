432a976943314d307f7dfb6a0623882f38d070b4
said1854 <msaliogullari@gmail.com>
2022-06-02 22:32:42 +0300

    first tr commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function () {
+    let randomNumber = Math.floor(Math.random() * 100);
+    let input1 = document.querySelector("#yourName").value.toLowerCase();
+    let input2 = document.querySelector("#loveName").value.toLowerCase();
+
+
+    if (input1 == "") {
+        alert("Kendı adını gir")
+    } else if (input2 === "") {
+        alert("Onun adını gir")
+    } else if (input1 == "said" && input2 == "alev") {
+        document.querySelector("#demo").textContent = "100";
+    } else if (input1 == "alev" && input2 == "said") {
+        document.querySelector("#demo").textContent = "100";
+    } else if (input1 != "" && input2 != "") {
+        console.log(input1);
+        console.log(input2);
+        document.querySelector("#demo").textContent = randomNumber + "%";
+    }
+
+
+
+});
